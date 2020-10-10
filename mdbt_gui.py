faaf3fda681b224906bd048edb987be7a2e6d955
Michael Pagliaro <mhp5035@rit.edu>
2020-10-09 22:03:32 -0400

    Separated the scrollable frame into its own file.

--- mdbt_gui.py
+++ mdbt_gui.py
@@ -1,5 +1,5 @@
 """
-mdbt.py
+mdbt_gui.py
 Author: Michael Pagliaro
 The graphical user interface. This is one possible entry-point to the application which gives users a visual
 interface for the program.
@@ -9,92 +9,8 @@ interface for the program.
 import tkinter as tk
 from tkinter import ttk
 import fileview
+import scrollable_frame as sf
 import configuration
-
-
-class ScrollableFrame(ttk.Frame):
-    """
-    Adapted from code by Jose Salvatierra (https://blog.tecladocode.com/tkinter-scrollable-frames/).
-    Creates a frame that can scroll if the amount of objects grows greater than the window height.
-    """
-
-    def __init__(self, container, initial_width=0, initial_height=-1, dynamic_width=True, **kwargs):
-        """
-        Create the object as a subclass of Frame.
-        :param container: The widget that houses this scrollable frame.
-        :param initial_width: The width in pixels this frame will start as. If no value is provided, this will
-                              wrap to what is specified when packed.
-        :param initial_height: The height in pixels this frame will start as. If no value is provided, this will
-                               wrap to what is specified when packed.
-        :param dynamic_width: Indicates if this frame should change width based on its child widgets. If true,
-                              when a new widget is added and its width is greater than the set width, the width
-                              will grow. True by default.
-        :param kwargs: Any other optional parameters to send to the super constructor.
-        """
-        super().__init__(container, **kwargs)
-        self.widgets = []
-        self.max_width = initial_width
-        self.dynamic_width = dynamic_width
-
-        # Create the overall canvas, the scrollbar, and the frame
-        self.canvas = tk.Canvas(self)
-        if self.max_width >= 0:
-            self.canvas.configure(width=self.max_width)
-        if initial_height >= 0:
-            self.canvas.configure(height=initial_height)
-        self.scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.canvas.yview)
-        self.scrollable_frame = ttk.Frame(self.canvas)
-
-        # Bind functions to the frame and canvas for resizing
-        self.scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
-        self.canvas.bind("<Configure>", self.on_resize)
-
-        # Pack all widgets
-        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor=tk.NW)
-        self.canvas.configure(yscrollcommand=self.scrollbar.set)
-        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-    @property
-    def master_create(self):
-        """
-        A property to specify in child widgets created for this frame. This should be specified as the
-        container argument for any new widget.
-        :return: The frame that will hold all child widgets.
-        """
-        return self.scrollable_frame
-
-    def update_width(self, new_width):
-        """
-        Update the width of the canvas to a new value if that new value is larger than the existing max width.
-        This should only be called if dynamic_width is true.
-        :param new_width: The new width to attempt to update to.
-        """
-        if new_width > self.max_width:
-            self.max_width = new_width
-            self.canvas.configure(width=self.max_width)
-
-    def on_resize(self, event):
-        """
-        Function called when this frame is resized. This will update the width of all child widgets to
-        fit the new size of the frame, except if they are Buttons.
-        :param event: The resize event.
-        """
-        for widget in self.widgets:
-            new_width = event.width-self.scrollbar.winfo_width()
-            # Not a good solution, but button width is text-based while other widgets are pixel-based
-            if not isinstance(widget, tk.Button):
-                widget.configure(width=new_width)
-
-    def register_widget(self, widget):
-        """
-        Register a new widget with this frame. This will allow it to be updated automatically on resize events.
-        This should be called after a new widget is created in this frame.
-        :param widget: The widget to be added.
-        """
-        self.widgets.append(widget)
-        if self.dynamic_width:
-            self.update_width(widget.winfo_width())
 
 
 class Application:
@@ -155,7 +71,7 @@ class Application:
         """
         Create the frame for buttons on the left side of the window which will represent configuration entries.
         """
-        self.entries_frame = ScrollableFrame(self.base)
+        self.entries_frame = sf.ScrollableFrame(self.base)
         button = tk.Button(self.entries_frame.master_create, text="New Entry")
         button.pack(ipadx=10, ipady=10)
         self.entries_frame.master.update()
@@ -204,9 +120,9 @@ class Application:
         """
         Create the scrollable frames that will hold input and output paths.
         """
-        self.input_frame = ScrollableFrame(self.tab_inputs, initial_width=-1, initial_height=50, dynamic_width=False)
+        self.input_frame = sf.ScrollableFrame(self.tab_inputs, initial_width=-1, initial_height=50, dynamic_width=False)
         self.input_frame.grid(column=0, row=1, sticky=tk.NSEW)
-        self.output_frame = ScrollableFrame(self.tab_inputs, initial_width=-1, initial_height=50, dynamic_width=False)
+        self.output_frame = sf.ScrollableFrame(self.tab_inputs, initial_width=-1, initial_height=50, dynamic_width=False)
         self.output_frame.grid(column=1, row=1, sticky=tk.NSEW)
 
     def configure_grid(self):
---
+++ scrollable_frame.py
@@ -0,0 +1,94 @@
+"""
+scrollable_frame.py
+Author: Michael Pagliaro
+A UI element that creates a frame with a scrollbar. Adapted from code found online at
+https://blog.tecladocode.com/tkinter-scrollable-frames/ by Jose Salvatierra.
+"""
+
+
+import tkinter as tk
+from tkinter import ttk
+
+
+class ScrollableFrame(ttk.Frame):
+    """
+    Creates a frame that can scroll if the amount of objects grows greater than the window height.
+    """
+
+    def __init__(self, container, initial_width=0, initial_height=-1, dynamic_width=True, **kwargs):
+        """
+        Create the object as a subclass of Frame.
+        :param container: The widget that houses this scrollable frame.
+        :param initial_width: The width in pixels this frame will start as. If no value is provided, this will
+                              wrap to what is specified when packed.
+        :param initial_height: The height in pixels this frame will start as. If no value is provided, this will
+                               wrap to what is specified when packed.
+        :param dynamic_width: Indicates if this frame should change width based on its child widgets. If true,
+                              when a new widget is added and its width is greater than the set width, the width
+                              will grow. True by default.
+        :param kwargs: Any other optional parameters to send to the super constructor.
+        """
+        super().__init__(container, **kwargs)
+        self.widgets = []
+        self.max_width = initial_width
+        self.dynamic_width = dynamic_width
+
+        # Create the overall canvas, the scrollbar, and the frame
+        self.canvas = tk.Canvas(self)
+        if self.max_width >= 0:
+            self.canvas.configure(width=self.max_width)
+        if initial_height >= 0:
+            self.canvas.configure(height=initial_height)
+        self.scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.canvas.yview)
+        self.scrollable_frame = ttk.Frame(self.canvas)
+
+        # Bind functions to the frame and canvas for resizing
+        self.scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+        self.canvas.bind("<Configure>", self.on_resize)
+
+        # Pack all widgets
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor=tk.NW)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    @property
+    def master_create(self):
+        """
+        A property to specify in child widgets created for this frame. This should be specified as the
+        container argument for any new widget.
+        :return: The frame that will hold all child widgets.
+        """
+        return self.scrollable_frame
+
+    def update_width(self, new_width):
+        """
+        Update the width of the canvas to a new value if that new value is larger than the existing max width.
+        This should only be called if dynamic_width is true.
+        :param new_width: The new width to attempt to update to.
+        """
+        if new_width > self.max_width:
+            self.max_width = new_width
+            self.canvas.configure(width=self.max_width)
+
+    def on_resize(self, event):
+        """
+        Function called when this frame is resized. This will update the width of all child widgets to
+        fit the new size of the frame, except if they are Buttons.
+        :param event: The resize event.
+        """
+        for widget in self.widgets:
+            new_width = event.width-self.scrollbar.winfo_width()
+            # Not a good solution, but button width is text-based while other widgets are pixel-based
+            if not isinstance(widget, tk.Button):
+                widget.configure(width=new_width)
+
+    def register_widget(self, widget):
+        """
+        Register a new widget with this frame. This will allow it to be updated automatically on resize events.
+        This should be called after a new widget is created in this frame.
+        :param widget: The widget to be added.
+        """
+        self.widgets.append(widget)
+        if self.dynamic_width:
+            self.update_width(widget.winfo_width())
