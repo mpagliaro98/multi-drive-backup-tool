3e360a9be7b627356c6ba18a2396761fb505ae22
Michael Pagliaro <mhp5035@rit.edu>
2020-10-11 14:05:25 -0400

    Functionality for creating and editing input paths.

--- mdbt_gui.py
+++ mdbt_gui.py
@@ -8,11 +8,42 @@ interface for the program.
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 import os
 import webbrowser
 import fileview
 import scrollable_frame as sf
 import configuration
+from configuration import InvalidPathException, SubPathException, CyclicEntryException
+
+
+def create_label_scrollable_frame(text, frame):
+    """
+    Create a label that will be put in a scrollable frame, using this program's settings for how labels
+    should look.
+    :param text: The text to display.
+    :param frame: The scrollable frame to put this text in.
+    """
+    input_label = tk.Message(frame.master_create, text=text)
+    input_label.pack()
+    frame.master.update()
+    frame.register_widget(input_label)
+
+
+def create_button_scrollable_frame(text, frame, command=lambda: None, ipadx=0, ipady=0):
+    """
+    Create a button that will be put in a scrollable frame, using this program's settings for how buttons
+    should look.
+    :param text: The text that goes in the button.
+    :param frame: The scrollable frame to add this button to.
+    :param command: Optional parameter, a function run when this button is pressed. Does nothing if not specified.
+    :param ipadx: Optional parameter, horizontal padding between the text and the button sides. Defaults to 0.
+    :param ipady: Optional parameter, vertical padding between the text and the button edges. Defaults to 0.
+    """
+    button = tk.Button(frame.master_create, text=text, command=command)
+    button.pack(ipadx=ipadx, ipady=ipady)
+    frame.master.update()
+    frame.register_widget(button)
 
 
 class Application:
@@ -98,7 +129,7 @@ class Application:
 
         # Create the buttons below the Fileviews
         self.input_button = tk.Button(self.tab_inputs, text="Set highlighted path to input",
-                                      command=lambda: print(self.input_tree.get_focus_path()))
+                                      command=self.set_input)
         self.input_button.grid(column=0, row=3)
         self.output_button = tk.Button(self.tab_inputs, text="Add highlighted path as output",
                                        command=lambda: print(self.output_tree.get_focus_path()))
@@ -178,11 +209,8 @@ class Application:
 
         # Create a button for every saved configuration
         for name_idx in range(len(self.config_name_list)):
-            button = tk.Button(config_load_frame.master_create, text=self.config_name_list[name_idx],
-                               command=lambda i=name_idx: load_window_response(self, i))
-            button.pack(ipadx=5, ipady=5)
-            config_load_frame.master.update()
-            config_load_frame.register_widget(button)
+            create_button_scrollable_frame(self.config_name_list[name_idx], config_load_frame,
+                                           command=lambda i=name_idx: load_window_response(self, i), ipadx=5, ipady=5)
         config_load_frame.pack(fill=tk.BOTH, expand=True)
         self.master.wait_window(self.load_window)
 
@@ -232,21 +260,14 @@ class Application:
         # Only display entry info if it's a valid entry number. Otherwise just clear the fields
         if 0 < entry_number <= self.config.num_entries():
             # Add the input path to the input scrollable frame
-            input_label = tk.Message(self.input_frame.master_create, text=self.config.get_entry(entry_number).input,
-                                     width=400)
-            input_label.pack()
-            self.input_frame.master.update()
-            self.input_frame.register_widget(input_label)
+            create_label_scrollable_frame(self.config.get_entry(entry_number).input, self.input_frame)
 
             # Set the input tree to display the input
             self.input_tree.travel_to_path(self.config.get_entry(entry_number).input)
 
             # Add every output path to the output scrollable frame
             for output in self.config.get_entry(entry_number).outputs:
-                output_label = tk.Message(self.output_frame.master_create, text=output, width=400)
-                output_label.pack()
-                self.output_frame.master.update()
-                self.output_frame.register_widget(output_label)
+                create_label_scrollable_frame(output, self.output_frame)
 
     def clear_fields(self):
         """
@@ -270,11 +291,8 @@ class Application:
         # Create a button for the given entry number
         path_split = os.path.split(self.config.get_entry(entry_number).input)
         button_text = path_split[0] if path_split[1] == "" else path_split[1]
-        button = tk.Button(self.entries_frame.master_create, text="Entry {}\n{}".format(entry_number, button_text),
-                           command=lambda: self.set_fields_to_entry(entry_number))
-        button.pack(ipadx=10, ipady=10)
-        self.entries_frame.master.update()
-        self.entries_frame.register_widget(button)
+        create_button_scrollable_frame("Entry {}\n{}".format(entry_number, button_text), self.entries_frame,
+                                       command=lambda: self.set_fields_to_entry(entry_number), ipadx=10, ipady=10)
 
         # Re-add the "New Entry" button
         self.add_new_entry_button()
@@ -283,11 +301,41 @@ class Application:
         """
         Create the "New Entry" button at the bottom of the entry button list.
         """
-        button = tk.Button(self.entries_frame.master_create, text="New Entry",
-                           command=lambda: self.set_fields_to_entry(self.config.num_entries()+1))
-        button.pack(ipadx=10, ipady=10)
-        self.entries_frame.master.update()
-        self.entries_frame.register_widget(button)
+        create_button_scrollable_frame("New Entry", self.entries_frame, command=lambda: self.set_fields_to_entry(
+            self.config.num_entries()+1), ipadx=10, ipady=10)
+
+    def set_input(self):
+        """
+        Functionality for the "set highlighted path as input" button. This will take the focused path from the
+        input Fileview and create a new configuration entry with it, or if an entry is already selected, this
+        will update this entry's input to the new path.
+        """
+        focus_path = self.input_tree.get_focus_path()
+        if focus_path == "":
+            return
+        # Create a new entry
+        if self.current_entry_number > self.config.num_entries():
+            try:
+                configuration.append_input_to_config(self.config, focus_path)
+                self.create_entry_button(self.current_entry_number)
+                create_label_scrollable_frame(self.config.get_entry(self.current_entry_number).input, self.input_frame)
+            except (InvalidPathException, CyclicEntryException) as e:
+                messagebox.showerror("Error", str(e))
+        # Edit an existing entry
+        else:
+            try:
+                configuration.edit_input_in_config(self.config, self.current_entry_number, focus_path)
+                path_split = os.path.split(self.config.get_entry(self.current_entry_number).input)
+                button_text = path_split[0] if path_split[1] == "" else path_split[1]
+                self.entries_frame.edit_text_on_widget(self.current_entry_number-1,
+                                                       "Entry {}\n{}".format(self.current_entry_number, button_text))
+
+                # Change the input path label
+                create_label_scrollable_frame(self.config.get_entry(self.current_entry_number).input, self.input_frame)
+                self.input_frame.remove_widget(0)
+            except (InvalidPathException, SubPathException, CyclicEntryException) as e:
+                messagebox.showerror("Error", str(e))
+        self.update_config_name_label()
 
 
 def main():
